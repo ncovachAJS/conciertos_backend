30ac77595fa10ae9a69d3dd5a062fecb6c4034fb
Nico <ncovach@gmail.com>
2026-07-06 19:47:06 +0200

    Railway

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
+  console.log(
+    `🚀 API iniciada en puerto ${process.env.PORT ?? 3000}`,
+  );
 }
 
 bootstrap();
