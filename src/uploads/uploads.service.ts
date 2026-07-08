e3194ffdda1f112b968354ccaf7a459b6fd0ee2e
Nico <ncovach@gmail.com>
2026-07-08 19:43:58 +0200

    imagen

--- src/uploads/uploads.service.ts
+++ src/uploads/uploads.service.ts
@@ -4,9 +4,12 @@ import { v2 as cloudinary } from 'cloudinary';
 @Injectable()
 export class UploadsService {
   constructor() {
-    console.log(process.env.CLOUDINARY_CLOUD_NAME);
-    console.log(process.env.CLOUDINARY_API_KEY);
-    console.log(!!process.env.CLOUDINARY_API_SECRET);
+    console.log('CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('API_KEY:', process.env.CLOUDINARY_API_KEY);
+console.log(
+  'API_SECRET:',
+  process.env.CLOUDINARY_API_SECRET ? 'OK' : 'NO',
+);
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
