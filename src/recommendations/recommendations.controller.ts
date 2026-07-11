0cc1224391bebbb1ddb10a249cda6d39043a74e0
Nico <ncovach@gmail.com>
2026-07-11 21:53:58 +0200

    *FIX recommendation

--- src/recommendations/recommendations.controller.ts
+++ src/recommendations/recommendations.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 import { RecommendationsDto } from './dto/recommendations.dto';
 import { RecommendationsService } from './recommendations.service';
@@ -10,6 +10,7 @@ export class RecommendationsController {
   ) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
   getRecommendations(
     @Body() dto: RecommendationsDto,
   ) {
