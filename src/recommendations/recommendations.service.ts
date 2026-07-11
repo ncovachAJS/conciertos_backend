06790c6de62f4ef2384d4d638d4eb6f9d387b876
Nico <ncovach@gmail.com>
2026-07-12 01:04:59 +0200

    *FIX recommendation

--- src/recommendations/recommendations.service.ts
+++ src/recommendations/recommendations.service.ts
@@ -33,60 +33,65 @@ export class RecommendationsService {
 }
 
   async getRecommendations(dto: RecommendationsDto) {
-    const { artists, countryCode } = dto;
+    const attractions = await this.findAttraction('Ghost');
 
-    const apiKey = this.config.get<string>('TICKETMASTER_API_KEY');
+  console.log(JSON.stringify(attractions, null, 2));
 
-    const url = 'https://app.ticketmaster.com/discovery/v2/events.json';
+  return [];
+    // const { artists, countryCode } = dto;
 
-    const responses: any[] = [];
+    // const apiKey = this.config.get<string>('TICKETMASTER_API_KEY');
 
-    console.log('Artistas recibidos:', artists);
+    // const url = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
-    const uniqueArtists = [...new Set(artists)];
+    // const responses: any[] = [];
 
-    for (const artist of uniqueArtists) {
-      console.log('Buscando:', artist);
-      try {
-        console.log(`🔍 Buscando ${artist}`);
+    // console.log('Artistas recibidos:', artists);
 
-        const response = await firstValueFrom(
-          this.http.get(url, {
-            params: {
-              apikey: apiKey,
-              keyword: artist,
-              classificationName: 'Music',
-              sort: 'date,asc',
-              size: 20,
-              ...(countryCode?.trim() ? { countryCode } : {}),
-            },
-          }),
-        );
+    // const uniqueArtists = [...new Set(artists)];
 
-        responses.push(response.data);
+    // for (const artist of uniqueArtists) {
+    //   console.log('Buscando:', artist);
+    //   try {
+    //     console.log(`🔍 Buscando ${artist}`);
 
-        console.log(JSON.stringify(response.data, null, 2));
-      } catch (error: any) {
-        console.error(
-          `❌ Error buscando ${artist}:`,
-          error.response?.status ?? error.message,
-        );
-      }
-    }
+    //     const response = await firstValueFrom(
+    //       this.http.get(url, {
+    //         params: {
+    //           apikey: apiKey,
+    //           keyword: artist,
+    //           classificationName: 'Music',
+    //           sort: 'date,asc',
+    //           size: 20,
+    //           ...(countryCode?.trim() ? { countryCode } : {}),
+    //         },
+    //       }),
+    //     );
 
-    const events = responses.flatMap(
-      (response) => response._embedded?.events ?? [],
-    );
+    //     responses.push(response.data);
 
-    return events.map((event: any) => ({
-      id: event.id,
-      artist: event.name,
-      venue: event._embedded?.venues?.[0]?.name ?? '',
-      city: event._embedded?.venues?.[0]?.city?.name ?? '',
-      country: event._embedded?.venues?.[0]?.country?.name ?? '',
-      date: event.dates?.start?.localDate,
-      imageUrl: event.images?.[0]?.url ?? '',
-      ticketUrl: event.url,
-    }));
+    //     console.log(JSON.stringify(response.data, null, 2));
+    //   } catch (error: any) {
+    //     console.error(
+    //       `❌ Error buscando ${artist}:`,
+    //       error.response?.status ?? error.message,
+    //     );
+    //   }
+    // }
+
+    // const events = responses.flatMap(
+    //   (response) => response._embedded?.events ?? [],
+    // );
+
+    // return events.map((event: any) => ({
+    //   id: event.id,
+    //   artist: event.name,
+    //   venue: event._embedded?.venues?.[0]?.name ?? '',
+    //   city: event._embedded?.venues?.[0]?.city?.name ?? '',
+    //   country: event._embedded?.venues?.[0]?.country?.name ?? '',
+    //   date: event.dates?.start?.localDate,
+    //   imageUrl: event.images?.[0]?.url ?? '',
+    //   ticketUrl: event.url,
+    // }));
   }
 }
