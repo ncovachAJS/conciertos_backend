c83aaea72d114247e9ed6c71013eda3cc3718e11
Nico <ncovach@gmail.com>
2026-07-15 20:35:27 +0200

    *FIX bugs

--- src/auth/dto/update-avatar.dto.ts
+++ src/auth/dto/update-avatar.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl } from 'class-validator';
 
 export class UpdateAvatarDto {
   @ApiProperty()
+  @IsString()
+  @IsUrl()
   imageUrl!: string;
 }
