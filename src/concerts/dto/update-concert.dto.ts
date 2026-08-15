021e1f097495a5efc6c45f476c22f61f33332faf
Nico <ncovach@gmail.com>
2026-08-15 10:21:13 +0200

    *FIX upgrade

--- src/concerts/dto/update-concert.dto.ts
+++ src/concerts/dto/update-concert.dto.ts
@@ -1,4 +1,35 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
 import { CreateConcertDto } from './create-concert.dto';
 
-export class UpdateConcertDto extends PartialType(CreateConcertDto) {}
+export class UpdateConcertDto extends PartialType(CreateConcertDto) {
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  override soundRating?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  override atmosphereRating?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  override setlistRating?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  override valueRating?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(5)
+  override artistRating?: number;
+}
